6c760759afc8efd8256bb6522d9a37b5caae1270
pad01g <101613202+pad01g@users.noreply.github.com>
2022-12-05 11:31:34 +0000

    add test to workflow

--- tests/components/Clip.test.jsx
+++ tests/components/Clip.test.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { render, screen } from "@testing-library/react";
-import * as _runTestHelper from '../utils/helper';
-import Clip from "../../src/components/Clip";
+import { render, screen } from '@testing-library/react';
+import Clip from '../../src/components/Clip';
 
-describe("clip component", () => {
-    beforeAll(() => {
-    })
-    test("should show table header", () => {
-        render(<table><Clip heading={true} /></table>);
-        expect(screen.getAllByText(/clip/).length).toBeTruthy()
-    });
+describe('clip component', () => {
+  beforeAll(() => {});
+  test('should show table header', () => {
+    render(
+      <table>
+        <Clip heading={true} />
+      </table>,
+    );
+    expect(screen.getAllByText(/clip/).length).toBeTruthy();
+  });
 });
--- tests/utils/helper.js
+++ tests/utils/helper.js
@@ -1,11 +1,12 @@
 jest.mock('react-polyglot', () => {
-    const originalModule = jest.requireActual('react-polyglot');
-    return {
-        __esModule: true,
-        ...originalModule,
-        useTranslate: jest.fn(() => {
-            return (key) => key
-        }),
-    };
+  const originalModule = jest.requireActual('react-polyglot');
+  return {
+    __esModule: true,
+    ...originalModule,
+    useTranslate: jest.fn(() => {
+      return (key) => key;
+    }),
+  };
 });
-export default {}
+const helper = {};
+export default helper;
