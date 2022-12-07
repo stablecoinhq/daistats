8e22bfe771d537acea96a605fa0a1a2183c490f1
pad01g <101613202+pad01g@users.noreply.github.com>
2022-12-07 06:49:36 +0000

    use act for render

--- tests/components/AuctionParticipants.test.jsx
+++ tests/components/AuctionParticipants.test.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import AuctionParticipants from '../../src/components/AuctionParticipants';
 
 describe('AuctionParticipants component', () => {
-  beforeAll(() => {});
-  test('should chart container', () => {
+  beforeAll(() => { });
+  test('should show empty chart', () => {
     const props = {
       subgraphClient: {
         request: async () => {
@@ -15,6 +15,25 @@ describe('AuctionParticipants component', () => {
       },
     };
     render(<AuctionParticipants {...props} />);
+    expect(screen.getAllByText(/auction_participants_chart/).length).toBeTruthy();
+  });
+  test('should show non-empty chart', async () => {
+    const props = {
+      subgraphClient: {
+        request: async () => {
+          return {
+            saleAuctions: [
+              { boughtAt: 1, userIncentives: { id: "0x02" }, userTaker: "0x01" },
+              { boughtAt: 1, userIncentives: { id: "0x02" }, userTaker: "0x03" },
+              { boughtAt: 2, userIncentives: { id: "0x02" }, userTaker: "0x04" },
+              { boughtAt: 3, userIncentives: { id: "0x03" }, userTaker: "0x01" },
+              { boughtAt: 3, userIncentives: { id: "0x02" }, userTaker: "0x04" },
+            ],
+          };
+        },
+      },
+    };
+    await act(async () => render(<AuctionParticipants {...props} />));
     expect(screen.getAllByText(/auction_participants_chart/).length).toBeTruthy();
   });
 });
