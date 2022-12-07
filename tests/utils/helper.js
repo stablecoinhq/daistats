c8420d60f7b6da915d8c5eae1dbd6c59c4595e4f
pad01g <101613202+pad01g@users.noreply.github.com>
2022-12-07 00:12:31 +0000

    contain mock inside function

--- tests/utils/helper.js
+++ tests/utils/helper.js
@@ -1,12 +1,15 @@
-jest.mock('react-polyglot', () => {
-  const originalModule = jest.requireActual('react-polyglot');
-  return {
-    __esModule: true,
-    ...originalModule,
-    useTranslate: jest.fn(() => {
-      return (key) => key;
-    }),
-  };
-});
+const prepareMocks = () => {
+  jest.mock('react-polyglot', () => {
+    const originalModule = jest.requireActual('react-polyglot');
+    return {
+      __esModule: true,
+      ...originalModule,
+      useTranslate: jest.fn(() => {
+        return (key) => key;
+      }),
+    };
+  });
+};
+prepareMocks();
 const helper = {};
 export default helper;
