7ab0f60e9f6805fbd81a8f594e4374f63c5701ba
pad01g <101613202+pad01g@users.noreply.github.com>
2022-12-08 02:32:06 +0000

    fix lint

--- tests/components/VoteHistoryLogTable.test.jsx
+++ tests/components/VoteHistoryLogTable.test.jsx
@@ -3,30 +3,34 @@ import { render, screen, act } from '@testing-library/react';
 import VoteHistoryLogTable from '../../src/components/VoteHistoryLogTable';
 
 describe('VoteHistoryLogTable component', () => {
-  beforeAll(() => { });
+  beforeAll(() => {});
   test('empty', async () => {
-    await act(async () => render(
-      <table>
-        <VoteHistoryLogTable heading={true} />
-      </table>,
-    ));
+    await act(async () =>
+      render(
+        <table>
+          <VoteHistoryLogTable heading={true} />
+        </table>,
+      ),
+    );
     expect(screen.getAllByText(/daistats.vote_history.description/).length).toBeTruthy();
   });
   test('some data', async () => {
     const props = {
       log: {
         timestamp: 12345678,
-        transaction: "0x01",
-        description: "some user voted"
-      }
-    }
-    await act(async () => render(
-      <table>
-        <tbody>
-          <VoteHistoryLogTable heading={false} {...props} />
-        </tbody>
-      </table>,
-    ));
+        transaction: '0x01',
+        description: 'some user voted',
+      },
+    };
+    await act(async () =>
+      render(
+        <table>
+          <tbody>
+            <VoteHistoryLogTable heading={false} {...props} />
+          </tbody>
+        </table>,
+      ),
+    );
     expect(screen.getAllByText(/some user voted/).length).toBeTruthy();
   });
 });
